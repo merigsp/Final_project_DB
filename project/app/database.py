8f9d94bd3e3d2d1553e0b391700605d9c1d39bdc
merigsp <meri.gasparyan05@yahoo.com>
2025-12-25 15:44:57 +0400

    db

--- project/app/database.py
+++ project/app/database.py
@@ -1,16 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/mydb")
+DATABASE_URL = "postgresql+psycopg2://user:password@db:5432/mydb"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
